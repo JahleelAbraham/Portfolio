82154f6d5e956cc7bea3bba47e49e8e84989e726
Jahleel Abraham <jahleelabraham@hotmail.com>
2023-05-02 18:39:27 +0100

    Start Machine Animation

---
+++ src/components/Theme.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useEffect } from 'react'
+
+function Theme() {
+  const [theme, setTheme] = React.useState(localStorage.getItem('theme') ?? 'night');
+  const [checkState, setCheckState] = React.useState(localStorage.getItem('theme') === 'night' ? true : false)
+
+  const toggleTheme = () => {
+    setTheme(theme === 'night' ? 'light' : 'night');
+    setCheckState(checkState ? false : true)
+  };
+
+  useEffect(() => {
+    document.querySelector('html')?.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+  return (
+    <input type="checkbox" className="toggle toggle-lg" checked={checkState} onClick={toggleTheme} />
+  );
+}
+
+export default Theme;
