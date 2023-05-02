125f034571133cf32de1eecbc1269cc5b4723f68
Jahleel Abraham <jahleelabraham@hotmail.com>
2023-05-02 18:40:19 +0100

    Pretteir Run

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { useEffect } from 'react'
+import React from "react";
+import { useEffect } from "react";
 
 function Theme() {
-  const [theme, setTheme] = React.useState(localStorage.getItem('theme') ?? 'night');
-  const [checkState, setCheckState] = React.useState(localStorage.getItem('theme') === 'night' ? true : false)
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ?? "night"
+  );
+  const [checkState, setCheckState] = React.useState(
+    localStorage.getItem("theme") === "night" ? true : false
+  );
 
   const toggleTheme = () => {
-    setTheme(theme === 'night' ? 'light' : 'night');
-    setCheckState(checkState ? false : true)
+    setTheme(theme === "night" ? "light" : "night");
+    setCheckState(checkState ? false : true);
   };
 
   useEffect(() => {
-    document.querySelector('html')?.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
+    document.querySelector("html")?.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
   return (
-    <input type="checkbox" className="toggle toggle-lg" checked={checkState} onClick={toggleTheme} />
+    <input
+      type="checkbox"
+      className="toggle toggle-lg"
+      checked={checkState}
+      onClick={toggleTheme}
+    />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import './index.css';
-import React from 'react';
-import Index from './pages/Index';
-import ReactDOM from 'react-dom/client';
+import "./index.css";
+import React from "react";
+import Index from "./pages/Index";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
